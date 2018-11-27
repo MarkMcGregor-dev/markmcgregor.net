2b708c2d57dc8bdf2eac4058418240501c796ba0
Mark McGregor <markmcgregor.contact@gmail.com>
2018-11-26 21:39:31 -0500

    Add getEC2Status function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Initialize the Amazon Cognito credentials provider
 AWS.config.region = 'us-east-2';
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-2:46d819bf-9ce9-4060-8a57-9c41a8322ecd',});
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-2:46d819bf-9ce9-4060-8a57-9c41a8322ecd',
+});
+AWS.config.apiVersions = {
+    ec2: '2016-11-15',
+    lambda: '2015-03-31',
+};
 
 function startup() {
     updateFields();
-    startEC2();
+    getEC2Status();
 }
 
 function updateFields() {
@@ -29,8 +35,23 @@ function setField(field, value) {
     document.getElementsByClassName(field)[0].innerHTML = value;
 }
 
+function getEC2Status() {
+    var ec2 = new AWS.EC2();
+
+    var params = {
+        InstanceIds: [
+            "i-07a157bcc0241d75c"
+        ]
+    };
+
+    ec2.describeInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+    });
+}
+
 function startEC2() {
-    var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+    var lambda = new AWS.Lambda();
     
     var params = {
         FunctionName: "StartEC2Instances", 
