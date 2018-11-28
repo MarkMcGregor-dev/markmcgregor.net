33b9cec1c90d180baecc7119e18692a707e20124
Mark McGregor <markmcgregor.contact@gmail.com>
2018-11-27 20:13:51 -0500

    Fix margin on title

--- script.js
+++ script.js
@@ -59,6 +59,6 @@ function startEC2() {
 
     lambda.invoke(params, function(err, data) {
         if (err) console.log(err, err.stack);
-        else console.log(data);
+        console.log(data);
     });
 }
